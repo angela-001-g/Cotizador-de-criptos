36af948395142b9b6593deb33c434c78ff2718f2
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-08-16 00:34:32 -0500

    stiling resultado

--- criptos-react/src/components/Resultado.jsx
+++ criptos-react/src/components/Resultado.jsx
@@ -1,18 +1,50 @@
 /* eslint-disable react/prop-types */
 import styled from "@emotion/styled"
 
+const Result = styled.div`
+    color: #FFF;
+    font-family: 'Lato', sans-serif;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-top: 30px;
+`
 
+const Imagen = styled.img`
+    display: block;
+    width: 120px;
+`
+
+const Texto = styled.p`
+    font-size: 18px;
+    span {
+        font-weight: 700;
+    }
+`
+
+const Precio = styled.p`
+    font-size: 24px;
+    span {
+        font-weight: 700;
+    }
+`
 
 function Resultado({resultado}) {
    const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE} = resultado
   return (
-    <div>
-        <p>El precio es de: <span>{PRICE}</span> </p>
-        <p>El precio más alto del día: <span>{HIGHDAY}</span> </p>
-        <p>El precio más bajo del día: <span>{LOWDAY}</span> </p>
-        <p>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span> </p>
-        <p>Última actualización: <span>{LASTUPDATE}</span> </p>
-    </div>
+    <Result>
+        <Imagen 
+            src={`https://cryptocompare.com/${IMAGEURL}`} 
+            alt="Imagen Cripto"
+        />
+        <div>
+            <Precio>El precio es de: <span>{PRICE}</span> </Precio>
+            <Texto>El precio más alto del día: <span>{HIGHDAY}</span> </Texto>
+            <Texto>El precio más bajo del día: <span>{LOWDAY}</span> </Texto>
+            <Texto>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span> </Texto>
+            <Texto>Última actualización: <span>{LASTUPDATE}</span> </Texto>
+        </div>
+    </Result>
 
   )
 }
